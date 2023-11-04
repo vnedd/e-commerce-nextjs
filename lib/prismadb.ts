0c4ff5aeb5f8ea096fb391923c504194b3c7a366
vnedd <thangvu2098@gmail.com>
2023-11-04 23:40:35 +0700

    fixed deloy

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,6 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const client = global.prismadb || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') global.prismadb = client
+declare global {
+    var prisma: PrismaClient | undefined;
+}
 
-export default client
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
